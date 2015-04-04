f0534b3f08d7fd4e9e04d0a797eba21ee168c177
Thor Smith <smith777@purdue.edu>
2015-04-03 21:05:19 -0400

    Collision detection between dots and Pacman has been added. Dots now dissapear when Pacman passes near enough to eat them. The collision detection is incredibly efficient in terms of speed. The dots are hashed based on their location in the map.

--- src/PacmanGame.java
+++ src/PacmanGame.java
@@ -9,6 +9,9 @@ import javax.swing.JFrame;
 import javax.swing.Timer;
 
 import java.awt.event.ActionListener;
+import java.util.List;
+import java.util.ArrayList;
+import java.lang.Math;
 
 class PacmanCanvas extends JComponent implements ActionListener, KeyListener {
 
@@ -22,6 +25,9 @@ class PacmanCanvas extends JComponent implements ActionListener, KeyListener {
     // Maze width and height
     public static final int MW = 27;
     public static final int MH = 16;
+    // Grid width/height
+    public static final int GRIDW = 30;
+    public static final int GRIDH = 30;
     // Start x,y of maze on maze image
     public static final int SX = 0;
     public static final int SY = 60;
@@ -50,6 +56,10 @@ class PacmanCanvas extends JComponent implements ActionListener, KeyListener {
     // There are 164 dots. Subtract 4 for ghosts,
     // 2 for empty positions above ghosts, and
     // subtract one for pacman.
+
+    // TODO: Create a tree of dots and perform collision detection
+    // on the dots.
+    Dot[] dotMap = new Dot[m.length];
     Dot[] dots = new Dot[157];
     Image maze = Toolkit.getDefaultToolkit().getImage("../images/maze.png");
     Pacman pacman = new Pacman(Toolkit.getDefaultToolkit().getImage("../images/pacman.png"), 360, 420);
@@ -76,6 +86,7 @@ class PacmanCanvas extends JComponent implements ActionListener, KeyListener {
                            (pos_x == 360 && pos_y == 240 );
                     if (skip == false) {
                         dots[k] = new Dot(pos_x, pos_y);
+                        dotMap[hashLocation(pos_x,pos_y)] = dots[k];
                         k++;
                     }
                 }
@@ -121,24 +132,28 @@ class PacmanCanvas extends JComponent implements ActionListener, KeyListener {
             if (this.m[MW*((pacman.y-1-SY)/30) + pacman.x/30] == 0) {
                 pacman.y -= 1;
                 pacman.animate(Pacman.UP);
+                hideDots(pacman);
             }
         } else if (keyCode == KEY_DOWN) {
             //System.out.println("Key Down.");
             if (this.m[MW*((pacman.y+30-SY)/30) + pacman.x/30] == 0) {
                 pacman.y += 1;
                 pacman.animate(Pacman.DOWN);
+                hideDots(pacman);
             }
         } else if (keyCode == KEY_RIGHT) {
             //System.out.println("Key Right.");
             if (this.m[MW*((pacman.y-SY)/30) + (pacman.x+30)/30] == 0) {
                 pacman.x += 1;
                 pacman.animate(Pacman.RIGHT);
+                hideDots(pacman);
             }
         } else if (keyCode == KEY_LEFT) {
             //System.out.println("Key Left.");
             if (this.m[MW*((pacman.y-SY)/30) + (pacman.x-1)/30] == 0) {
                 pacman.x -= 1;
                 pacman.animate(Pacman.LEFT);
+                hideDots(pacman);
             }
         }
     }
@@ -148,6 +163,62 @@ class PacmanCanvas extends JComponent implements ActionListener, KeyListener {
     }
     public void keyTyped(KeyEvent e) {
         //System.out.println("Key Typed.");
+    }
+
+    private int hashLocation(int x, int y) {
+        return ((y-SY)/GRIDH)*MW + (x-SX)/GRIDW;
+    }
+
+    private List<Dot> collided(int xpos, int ypos, int w, int h) {
+        ArrayList<Dot> dotList = new ArrayList<Dot>();
+        final int area = w*h;
+        final int numBlocks = area/(GRIDW*GRIDH);
+        // check near edges and middle
+        for (int x = xpos; x < xpos + w; x+=GRIDW) {
+            for (int y = ypos; y < ypos + h; y += GRIDH) {
+                Dot myDot = dotMap[hashLocation(x,y)];
+                if (myDot != null) {
+                    dotList.add(myDot);
+                }
+            }
+        }
+        // Check far edges of item.
+        for (int x = xpos; x < xpos + w; x+=GRIDW) {
+            int y = ypos + h;
+            Dot myDot = dotMap[hashLocation(x,y)];
+            if (myDot != null) {
+                dotList.add(myDot);
+            }
+        }
+        for (int y = ypos; y < ypos + h; y+=GRIDH) {
+            int x = xpos + w;
+            Dot myDot = dotMap[hashLocation(x,y)];
+            if (myDot != null) {
+                dotList.add(myDot);
+            }
+        }
+
+        return dotList;
+    }
+
+    private void hideDots(Pacman pac) {
+        List<Dot> dotList = collided(pac.x, pac.y, pac.w, pac.h);
+        //System.out.println("Collided dots: ");
+        for (Dot d : dotList) {
+            int p_x = pac.x + pac.w/2;
+            //System.out.println("pac.x: " + pac.x + " pac.w: " + pac.w + "p_x: " + p_x );
+            int p_y = pac.y + pac.h/2;
+            //System.out.println("pac.y: " + pac.y + " pac.h: " + pac.h + "p_y: " + p_y );
+            int d_x = d.x + d.w/2;
+            //System.out.println("d.x: " + d.x + " d.x: " + d.w + "d_x: " + d_x );
+            int d_y = d.y + d.h/2;
+            //System.out.println("d.y: " + d.y + " d.y: " + d.h + "d_y: " + d_y );
+            int dist = (int) Math.sqrt((p_x - d_x)*(p_x - d_x) + (p_y - d_y)*(p_y - d_y));
+            //System.out.println("Dist: " + dist);
+            if (dist < d.w / 2) {
+                d.hide();
+            }
+        }
     }
 }
 
@@ -198,6 +269,7 @@ class Ghost {
 class Dot {
     public static Image image = Toolkit.getDefaultToolkit().getImage("../images/dot.png");
     public int x,y;
+    public int w=30, h=30;
     public boolean show = true;
 
     public Dot(int x, int y) {
